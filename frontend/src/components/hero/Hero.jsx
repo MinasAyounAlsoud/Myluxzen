32070e9ade3310c3478f8a268dc3e24d58a2664d
[f.zahra] <fatima.azekraoui@dci-student.org>
2025-03-27 10:30:26 +0100

    add client anfrage

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -11,6 +11,7 @@ const Hero = () => {
   const [parallaxEnabled, setParallaxEnabled] = useState(false);
   const hasAnimated = useRef(false);
 
+
   // Fonction pour initialiser les positions des éléments avant animation
   const initializeParallax = () => {
     const heroSection = heroRef.current;
@@ -127,34 +128,41 @@ const Hero = () => {
     };
   }, [parallaxEnabled]);
 
+
   return (
     <div className="hero-container" ref={heroRef}>
     <main>
-      <div className="vignette hide"></div>
-      <img src={assetsHome.background} loading="lazy" data-speedx="0" data-speedy="0.2" data-speedz="0" data-rotation="0" className="animation bg-img" />
-      <img src={assetsHome.fog_7} data-speedx="0.27" data-speedy="0.32" data-speedz="0" data-rotation="0.1" className="animation fog-7" />
-      <img src={assetsHome.mountain_10} loading="lazy" data-speedx="0.195" data-speedy="0.305" data-speedz="0" data-rotation="0.2" className="animation mountain-10" />
-      <img src={assetsHome.fog_6} data-speedx="0" data-speedy="0.28" data-speedz="0" data-rotation="0.05" className="animation fog-6" />
-      <img src={assetsHome.mountain_9} data-speedx="0.125" data-speedy="0.155" data-speedz="0.15" data-rotation="0.15" className="animation mountain-9" />
-      <img src={assetsHome.mountain_8} loading="lazy" data-speedx="0.1" data-speedy="0.11" data-speedz="0" data-rotation="0.05" className="animation mountain-8" />
-      <img src={assetsHome.fog_5} data-speedx="0.16" data-speedy="0.105" data-speedz="0" data-rotation="0.08" className="animation fog-5" />
-      <img src={assetsHome.mountain_7} loading="lazy" data-speedx="0.1" data-speedy="0.1" data-speedz="0" data-rotation="0.12" className="animation mountain-7" />
-      <div className="text animation">
-        <h2>Welcome to</h2>
-        <h1>MyLuXZeN</h1>
-      </div>
-      <img src={assetsHome.mountain_6} loading="lazy" data-speedx="0.065" data-speedy="0.05" data-speedz="0.05" data-rotation="0.1" className="animation mountain-6" />
-      <img src={assetsHome.fog_4} data-speedx="0.135" data-speedy="0.1" data-speedz="0" data-rotation="0.07" className="animation fog-4" />
-      <img src={assetsHome.mountain_5} loading="lazy" data-speedx="0.08" data-speedy="0.04" data-speedz="0.13" data-rotation="0.15" className="animation mountain-5" />
-      <img src={assetsHome.fog_3} data-speedx="0.11" data-speedy="0.018" data-speedz="0" data-rotation="0.05" className="animation fog-3" />
-      <img src={assetsHome.mountain_4} loading="lazy" data-speedx="0.059" data-speedy="0.024" data-speedz="0" data-rotation="0.1" className="animation mountain-4" />
-      <img src={assetsHome.mountain_3} loading="lazy" data-speedx="0.04" data-speedy="0.018" data-speedz="0.32" data-rotation="0.2" className="animation mountain-3" />
-      <img src={assetsHome.fog_2} data-speedx="0.15" data-speedy="0.0115" data-speedz="0" data-rotation="0.1" className="animation fog-2" />
-      <img src={assetsHome.mountain_2} loading="lazy" data-speedx="0.0235" data-speedy="0.013" data-speedz="0.42" data-rotation="0.2" className="animation mountain-2" />
-      <img src={assetsHome.mountain_1} loading="lazy" data-speedx="0.027" data-speedy="0.018" data-speedz="0.53" data-rotation="0.25" className="animation mountain-1" />
-      <img src={assetsHome.sun_rays} className="sun-rays hide" />
-      <img src={assetsHome.fog_1} data-speedx="0" data-speedy="0.01" data-speedz="0" data-rotation="0.1" className="animation fog-1" />
-    </main>
+  <div className="vignette hide"></div>
+
+  {/* Ne pas mettre de scrollspeed ici */}
+  <img src={assetsHome.background} loading="lazy" data-speedx="0" data-speedy="0.2" data-speedz="0" data-rotation="0" className="animation bg-img" />
+
+  <img src={assetsHome.fog_7} data-scrollspeed="0.05" data-speedx="0.27" data-speedy="0.32" data-speedz="0" data-rotation="0.1" className="animation fog-7" />
+  <img src={assetsHome.mountain_10} loading="lazy" data-scrollspeed="0.07" data-speedx="0.195" data-speedy="0.305" data-speedz="0" data-rotation="0.2" className="animation mountain-10" />
+  <img src={assetsHome.fog_6} data-scrollspeed="0.04" data-speedx="0" data-speedy="0.28" data-speedz="0" data-rotation="0.05" className="animation fog-6" />
+  <img src={assetsHome.mountain_9} data-scrollspeed="0.09" data-speedx="0.125" data-speedy="0.155" data-speedz="0.15" data-rotation="0.15" className="animation mountain-9" />
+  <img src={assetsHome.mountain_8} loading="lazy" data-scrollspeed="0.1" data-speedx="0.1" data-speedy="0.11" data-speedz="0" data-rotation="0.05" className="animation mountain-8" />
+  <img src={assetsHome.fog_5} data-scrollspeed="0.06" data-speedx="0.16" data-speedy="0.105" data-speedz="0" data-rotation="0.08" className="animation fog-5" />
+  <img src={assetsHome.mountain_7} loading="lazy" data-scrollspeed="0.11" data-speedx="0.1" data-speedy="0.1" data-speedz="0" data-rotation="0.12" className="animation mountain-7" />
+
+  <div className="text animation">
+    <h2>Welcome to</h2>
+    <h1>MyLuXZeN</h1>
+  </div>
+
+  <img src={assetsHome.mountain_6} loading="lazy" data-scrollspeed="0.12" data-speedx="0.065" data-speedy="0.05" data-speedz="0.05" data-rotation="0.1" className="animation mountain-6" />
+  <img src={assetsHome.fog_4} data-scrollspeed="0.07" data-speedx="0.135" data-speedy="0.1" data-speedz="0" data-rotation="0.07" className="animation fog-4" />
+  <img src={assetsHome.mountain_5} loading="lazy" data-scrollspeed="0.14" data-speedx="0.08" data-speedy="0.04" data-speedz="0.13" data-rotation="0.15" className="animation mountain-5" />
+  <img src={assetsHome.fog_3} data-scrollspeed="0.06" data-speedx="0.11" data-speedy="0.018" data-speedz="0" data-rotation="0.05" className="animation fog-3" />
+  <img src={assetsHome.mountain_4} loading="lazy" data-scrollspeed="0.15" data-speedx="0.059" data-speedy="0.024" data-speedz="0" data-rotation="0.1" className="animation mountain-4" />
+  <img src={assetsHome.mountain_3} loading="lazy" data-scrollspeed="0.18" data-speedx="0.04" data-speedy="0.018" data-speedz="0.32" data-rotation="0.2" className="animation mountain-3" />
+  <img src={assetsHome.fog_2} data-scrollspeed="0.07" data-speedx="0.15" data-speedy="0.0115" data-speedz="0" data-rotation="0.1" className="animation fog-2" />
+  <img src={assetsHome.mountain_2} loading="lazy" data-scrollspeed="0.2" data-speedx="0.0235" data-speedy="0.013" data-speedz="0.42" data-rotation="0.2" className="animation mountain-2" />
+  <img src={assetsHome.mountain_1} loading="lazy" data-scrollspeed="0.22" data-speedx="0.027" data-speedy="0.018" data-speedz="0.53" data-rotation="0.25" className="animation mountain-1" />
+  <img src={assetsHome.sun_rays} className="sun-rays hide" />
+  <img src={assetsHome.fog_1} data-scrollspeed="0.05" data-speedx="0" data-speedy="0.01" data-speedz="0" data-rotation="0.1" className="animation fog-1" />
+</main>
+
   </div>
 );
 };
